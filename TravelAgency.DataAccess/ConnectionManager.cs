9b85ea48cc90f7696ce045f736c25856e4b8bb58
Amer Sawan <amersaw@gmail.com>
2016-12-08 23:43:00 +0300

    First Commit

---
+++ TravelAgency.DataAccess/ConnectionManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency.DataAccess
+{
+    public class ConnectionManager
+    {
+
+        private static SqlConnection _con = null;
+
+        public static SqlConnection GetConnection()
+        {
+            if (_con == null)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                _con = new SqlConnection(connectionString);
+            }
+            return _con;
+        }
+    }
+}
---
+++ TravelAgency.DataAccess/Provinces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAgency.Model;
+
+namespace TravelAgency.DataAccess
+{
+    public static class Provinces
+    {
+        public static List<Province> GetAll()
+        {
+            List<Province> retVal = new List<Province>();
+            var con = ConnectionManager.GetConnection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Province",con);
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                retVal.Add(new Province(rdr));
+            }
+            return retVal;
+        }
+    }
+}
