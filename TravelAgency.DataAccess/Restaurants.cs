8fad10072228e7bf2ca3ea928437060a3a9b6a24
Amer Sawan <amersaw@gmail.com>
2016-12-11 02:17:11 +0300

    add Restaurant

--- TravelAgency.DataAccess/Restaurants.cs
+++ TravelAgency.DataAccess/Restaurants.cs
@@ -14,5 +14,11 @@ namespace TravelAgency.DataAccess
             string sql = string.Format("SELECT * FROM Restaurants WHERE CityId={0}", cityId);
             return DBRetriever.RetrieveList<Restaurant>(sql);
         }
+
+        public static Restaurant GetById(int id)
+        {
+            string sql = string.Format("SELECT * FROM Restaurants WHERE Id={0}", id);
+            return DBRetriever.Retrieve<Restaurant>(sql);
+        }
     }
 }
---
+++ TravelAgency/Controllers/RestaurantController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelAgency.Model;
+
+namespace TravelAgency.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        // GET: Restaurant
+        public ActionResult Show(int id )
+        {
+            Restaurant r = DataAccess.Restaurants.GetById(id);
+            return View(r);
+        }
+    }
+}
